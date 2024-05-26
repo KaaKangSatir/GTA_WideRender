830cfa44fa31161f0f2fda136c39327c7e81b03a
KaaKangSatir <141976349+KaaKangSatir@users.noreply.github.com>
2024-05-26 20:31:37 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cpp .cc
-LOCAL_MODULE    := WideRender
+LOCAL_MODULE    := ThisModByKaaKangSatir
 LOCAL_SRC_FILES := main.cpp
 LOCAL_CFLAGS += -Ofast -mfloat-abi=softfp -DNDEBUG
 include $(BUILD_SHARED_LIBRARY)
