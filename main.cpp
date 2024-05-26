89caee7f9b979a05a7287181d9bfc3c4dca2d91f
KaaKangSatir <141976349+KaaKangSatir@users.noreply.github.com>
2024-05-26 20:56:28 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,22 +10,6 @@ END_DEPLIST()
 void *hGTASA;
 ISAUtils* sautils;
 
-const char* yesnoSwitch[2] = 
-{
-    "FEM_NO",
-    "FEM_YES"
-};
-const char* AspectRatioNames[] =
-{
-    "Auto (default)",
-    "3:2",
-    "4:3",
-    "5:4",
-    "16:9",
-    "16:10",
-    "21:9",
-    "32:9",
-};
 const float AspectRatios[sizeofA(AspectRatioNames)] =
 {
     0,
@@ -45,36 +29,18 @@ int bCorrectAspect = 1;
 DECL_HOOKv(CameraSize, void* camera, void* rect, float unk, float aspect)
 {
     float newaspect = AspectRatios[currentAspect];
-    if(newaspect == 0)
     {
         CameraSize(camera, rect, unk, aspect);
     }
     else
     {
-        CameraSize(camera, rect, unk, bCorrectAspect!=0 ? aspect / newaspect : newaspect);
+        CameraSize(camera, rect, unk, bCorrectAspect!=1 ? aspect / newaspect : newaspect);
     }
 }
 
-void OnARChange(int oldVal, int newVal, void* data)
-{
-    clampint(0, maxEls, &newVal);
-    currentAspect = newVal;
-    aml->MLSSetInt("ASPCTVL", newVal);
-}
-void OnARCorrectionChange(int oldVal, int newVal, void* data)
-{
-    clampint(0, 1, &newVal);
-    bCorrectAspect = newVal;
-    aml->MLSSetInt("ASPCTCR", newVal);
-}
 extern "C" void OnModLoad()
 {
     hGTASA = aml->GetLibHandle("libGTASA.so");
-    sautils = (ISAUtils*)GetInterface("SAUtils");
-    if(!hGTASA || !sautils) return;
-
-    aml->MLSGetInt("ASPCTVL", &currentAspect); clampint(0, maxEls, &currentAspect);
-    aml->MLSGetInt("ASPCTCR", &bCorrectAspect); clampint(0, 1, &bCorrectAspect);
 
     HOOKSYM(CameraSize, hGTASA, "_Z10CameraSizeP8RwCameraP6RwRectff");
 }
